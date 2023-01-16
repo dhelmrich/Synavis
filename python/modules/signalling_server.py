210b30fc8f552f62bc8cb821182e91ab4906d5be
Dirk Helmrich <dirk.helmrich@alumni.fh-aachen.de>
2023-01-16 19:10:01 +0100

    Signalling server now correctly uses websockets

--- python/modules/signalling_server.py
+++ python/modules/signalling_server.py
@@ -1,174 +1,192 @@
-from json import JSONDecoder, JSONEncoder
-from urllib import request
-from flask import Flask, render_template, request, session, abort, url_for, Response
-import eventlet
-import eventlet.wsgi
-import socketio
-#from flask_socketio import SocketIO, send, emit, join_room, leave_room
+import asyncio
+import websockets as ws
 import json
-from enum import Enum
+import time
+from colorama import init as colorama_init
+from colorama import Fore, Back, Style
 
-sio = socketio.Server()
-#app = Flask(__name__)
-app = socketio.WSGIApp(sio)
-#app.config['transports'] = 'websocket'
-#socketio = SocketIO(app, logger=True, engineio_logger=True, debug=True)
+ws.debug = True
 
-@sio.on('*')
-def catch_all(event, sid, data):
-  print("hehehe")
+server_port = 8888
+client_port = 8889
 
-#@app.route('/')
-def starter():
-  #return success
-  print("index was called: ")
-  print("Requested URL was ", request.base_url)
-  print("Origin ", request.origin)
-  return {}
+class Logger() :
+  def __init__ (self):
+    colorama_init()
+  #enddef
+  def log(self, message, color = Fore.WHITE) :
+    print(color + message + Style.RESET_ALL)
+  #enddef
+  def info(self, message) :
+    self.log(message, Fore.WHITE)
+  #enddef
+  def warn(self, message) :
+    self.log(message, Fore.YELLOW)
+  #enddef
+  def error(self, message) :
+    self.log(message, Fore.RED)
+  #enddef
+  def success(self, message) :
+    self.log(message, Fore.GREEN)
+  #enddef
 
-#@app.route('/main')
-def main():
-  #return success
-  print("index was called: ")
-  print("Requested URL was ", request.base_url)
-  print("Origin ", request.origin)
-  return {}
+  def log_outgoing(self, message) :
+    self.log("-> " + message, Fore.BLUE)
+  #enddef
+  def log_incoming(self, message, sender = None) :
+    if sender != None :
+      self.log(str(sender.role) + "[" + str(sender.id) + "]" + " <- " + str(message), Fore.CYAN)
+    else :
+      self.log("<- " + message, Fore.CYAN)
+  #enddef
 
-#@app.errorhandler(404)
-def page_not_found(error):
-  print("error was called: ", error)
-  print("Requested URL was ", request.base_url)
-  print("Origin ", request.origin)
-  exit(-1)
-  return '{"type":"fail"}', 404
+  def log_many(self, level = "info", *messages) :
+    cumulated = ""
+    for message in messages :
+      cumulated += message + " "
+    #endfor
+    if level == "info" :
+      self.info(cumulated)
+    elif level == "warn" :
+      self.warn(cumulated)
+    elif level == "error" :
+      self.error(cumulated)
+    elif level == "success" :
+      self.success(cumulated)
+#endclass
 
-class EndpointType(Enum) :
-  Streamer = 1
-  Client = 2
-  Matchmaker = 3
-  TURN = 4
-  Bridge = 5
+glog = Logger()
 
-class Endpoint :
-  def __init__(self, id, name) :
-    print("Created new Endpoint! ", id, " -> ", name)
-    self.id = id
-    self.name = name
-  #end construct
-#class Endpoint
+connections = {}
 
-class Media :
-  def __init__(self, name) :
-    self.name = name
-  #enddef construct
-#end class
+class Connection() :
+  def __init__(self, websocket) :
+    self.websocket = websocket
+    self.id = None
+    self.role = "server" if websocket.local_address[1] == server_port else "client"
+    self.assign_id()
+    self.recent = True
+    self.connected_ids = []
+    self.supports_extmap = False
+    self.supports_synchronous_data = False
+    self.extmap = []
+  #enddef
+  def send(self, message) :
+    self.websocket.send(message)
+  #enddef
+  def assign(self, other) :
+    self.connected_ids.append(other.id)
+    other.connected_ids.append(self.id)
+  #enddef
+  def unassign(self, other) :
+    self.connected_ids.remove(other.id)
+    # only we have to remove the id, because the other connection will be removed anyway
+  #enddef
+  def is_recent(self) :
+    if self.recent :
+      self.recent = False
+      return True
+    else :
+      return False
+  def close(self) :
+    self.websocket.close()
+  #enddef
+  """ a method that assigns a new unique id to the connection"""
+  def assign_id(self) :
+    self.id = 100
+    while self.id in connections :
+      self.id += 1
+    #endwhile	
+  #enddef
+  def __str__(self) :
+    return "Connection[" + str(self.id) + "]" + " (" + self.role + ")"
+  #enddef
+  def ParseSDP(self, spd: str) :
+    extmap_startswith = "a=extmap"
+    extmap_skiplength = len(extmap_startswith)
+    lines = spd.splitlines(False)
+    categories = []
+    medias = [i for i in range(len(lines)) if lines[i].startswith("m=")]
+    maplines = [[lines[i] for i in range(j,len(lines)) if lines[i].startswith(extmap_startswith)] for j in medias]
+    hpc_line = [line for line in maplines if "hpc-bridge" in line]
+    if len(maplines) > 0 :
+      self.supports_extmap = True
+    if len(hpc_line) > 0 :
+      self.supports_synchronous_data = True
+    for stream in medias :
+      if len(maplines[stream]) > 0 :
+        self.extmap.append[(lines[medias.split(0)[2:]], int(maplines.split()[0][extmap_skiplength:]))]
+    #endfor
+  #enddef
+#endclass
 
-def ParseSDP(spd: str) :
-  extmap_startswith = "a=extmap"
-  extmap_skiplength = len(extmap_startswith)
-  lines = spd.splitlines(False)
-  categories = []
-  medias = [i for i in range(len(lines)) if lines[i].startswith("m=")]
-  maplines = [[lines[i] for i in range(j,len(lines)) if lines[i].startswith(extmap_startswith) and "hpc-bridge" in lines[i]] for j in medias]
-  print(maplines)
-  for stream in medias :
-    if len(maplines[stream]) > 0 :
-      categories.append[(lines[medias.split(0)[2:]], int(maplines.split()[0][extmap_skiplength:]))]
-  return categories
-#enddef
-
-@sio.event
-def event_response(sid, env) :
-  print("hehe")
-  return '', 101
-
-@sio.on('join')
-def on_join(data):
-  print("A endpoint joined")
-  room = request.sid
-  join_room(room)
-  return '', 201
-#enddef
-
-
-@sio.on('leave')
-def on_leave(data):
-  print("An endpoint disconnected")
-  username = data['username']
-  room = data['room']
-  leave_room(room)
-  send(username + ' has left the room.', to=room)
-#enddef
-
-Sessions = {}
-
-PlayerSessions = []
-StreamerSessions = []
-Tracks = []
-
-@sio.on('connection')
-def test_connect(auth):
-  print("connected")
-  sio.emit('my response', {'data': 'Connected'})
-  id = request.sid
-  orig = request.origin
-  return '', 201
-#enddef
-
-@sio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected')
-#enddef
-
-@sio.on('message', namespace='/')
-def messgage(sid, data):
-  print("message")
-  sio.emit('message', data=data)
-
-@sio.on('json')
-def signalling(json):
-  content = json.decoder.JSONDecoder().decode(data)
-  print("-> Signalling: ", content)
+async def handle(connection, message) :
+  global connections, glog
+  content = {}
+  id = -1
+  try :
+    content = json.loads(message)
+  except :
+    glog.error("Invalid JSON: " + message)
+    return
   if "type" in content :
     if content["type"] == "ping" :
-      print("-> Server: Ping")
+      glog.log_incoming(content, connection)
+      glog.log_outgoing("pong")
+      content["type"] = "pong"
+      # setting the time to the current time in seconds
+      content["time"] = int(time.time())
+      await connection.websocket.send(json.dumps(content))
     #endif
-  #endif
-  id_entry = next(("id" in entry.toLowercase() for entry in content), -1)
-  if id_entry != -1 :
-    id = content[id_entry]
-    if "sdp" in content :
+    elif "sdp" in content :
       sdp = content["sdp"]
-      metamap = ParseSDP(sdp)
+      connection.ParseSDP(sdp)
       data = json()
       data["type"] = "extmap"
-      data["extmap"] = JSONEncoder(metamap)
-      for player in PlayerSessions :
-        sio.emit(data.dump(), room = player)
-    #endif
+      data["extmap"] = json.encoder(connection.extmap)
+      if connection.role == "server" :
+        for player_id in connection.connected_ids :
+          await connections[player_id].send(json.dumps(data))
+      elif connection.role == "client" :
+        for server_id in connection.connected_ids :
+          data["playerId"] = connection.id
+          await connections[server_id].send(json.dumps(data))
   #endif
-  return '', 201
 #enddef
 
-def test(env, start_response) :
-  print("Settings response for continue status codes")
-  env['wsgi.input'].set_hundred_continue_response_headers(
-    [('Hundred-Continue-Header-1', 'H1'),
-      ('Hundred-Continue-Header-k', 'Hk')])
-  print("Defining response")
-  start_response('101 Continue', [('Content-Type', 'text/json')])
-  print("Returning with status code")
-  return "{'type':'success'}"
+async def connection(websocket, path) :
+  global connections, glog
+  glog.log_many("info", "{" , str([id for id in connections]) , "}")
+  connection = next((connections[id] for id in connections if connections[id].websocket == websocket), Connection(websocket))
+  if connection.role == "client" :
+    await websocket.send(json.dumps({"type": "id", "id": connection.id}))
+  if connection.is_recent() :
+    glog.success("open connection " + str(connection))
+    connections[connection.id] = connection
+  #endif
+  while True :
+    try :
+      message = await asyncio.wait_for(websocket.recv(), timeout = 1.0)
+      await handle(connection, message)
+    except asyncio.exceptions.TimeoutError as t :
+      continue
+    except ws.WebSocketException as e :
+      if e.code in [1000,1001] :
+        glog.warn("Websocket closed: " + str(e))
+      else :
+        glog.error("Websocket error: " + str(e))
+      break
+  connections.pop(connection.id)
+  glog.info("closed connection " + str(connection))
+  del connection
+#endeif
 
+async def main() :
+  global connections, glog
+  async with ws.serve(connection, 'localhost', 8888), ws.serve(connection, 'localhost', 8889) :
+    await asyncio.Future()
 
-if __name__ == '__main__' :
-  #from waitress import serve
-  #serve(app, host="0.0.0.0", port=5000)
-  #mware = socketio.WSGIApp(sio, app)
-  eventlet.wsgi.server(eventlet.listen(('', 8888)), test)
-  #socketio.run(app, debug=True, host="127.0.0.1",port=8888)
-  #with open("../../sdp.txt","r") as f:
-  #  sdp = f.read()
-  #  ParseSDP(sdp)
-#endif main
-  
+if __name__ == "__main__" :
+  glog.info("Starting server...")
+  asyncio.run(main())
+
