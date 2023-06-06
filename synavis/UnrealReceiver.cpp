c80e01f51c78d8c27767fc906b21f12eb824cb6f
Dirk Helmrich <dnh2@hi.is>
2023-06-06 14:07:58 +0200

    Updated command in legacy code

--- synavis/UnrealReceiver.cpp
+++ synavis/UnrealReceiver.cpp
@@ -164,7 +164,7 @@ UnrealReceiver::UnrealReceiver()
       sendto(sock, reinterpret_cast<const char*>(package.data()), int(package.size()), 0,
         reinterpret_cast<const struct sockaddr*>(&addr), sizeof(addr));
 #elif __linux__
-      write(sock,reinterpret_cast<const void*>(package.data()),package.size());
+      send(sock,reinterpret_cast<const void*>(package.data()),package.size());
 #endif
       
       Messages.push_back(std::move(rtp));
