1abb7a9f15fa8b3a5572200a0eb7223165566d26
Dirk Helmrich <dnh2@hi.is>
2025-02-12 18:13:43 +0100

    Better output for video

--- python/coupling/characterization.py
+++ python/coupling/characterization.py
@@ -29,6 +29,7 @@ def message_callback(msg) :
   #print("Received message: ", msg)
   # decode from utf-8
   message_buffer.append(str(msg))
+  print(msg)
 
 # a callback function for the data connector
 def data_callback(data) :
@@ -39,8 +40,8 @@ def data_callback(data) :
 if sys.platform == "win32" :
   sys.path.append("../build_win/synavis/Release/")
 else :
-  sys.path.append("../unix/")
-sys.path.append("../modules/")
+  sys.path.append("./unix/")
+sys.path.append("./modules/")
 import PySynavis as rtc
 
 rtc.SetGlobalLogVerbosity(rtc.LogVerbosity.LogError)
@@ -48,7 +49,7 @@ rtc.SetGlobalLogVerbosity(rtc.LogVerbosity.LogError)
 #make the data connector
 dataconnector = rtc.DataConnector()
 dataconnector.Initialize()
-dataconnector.SetConfig({"SignallingIP": "127.0.0.1","SignallingPort":8080})
+dataconnector.SetConfig({"SignallingIP": "172.20.16.1","SignallingPort":8080})
 dataconnector.SetTakeFirstStep(True)
 dataconnector.StartSignalling()
 dataconnector.SetDataCallback(data_callback)
@@ -63,11 +64,13 @@ reset_message()
 def logscale(start, end, number) :
   return np.logspace(np.log10(start), np.log10(end), number)
 
-intensity_levels = logscale(13.50287, 18334254.0, 500)
-emissive_levels = logscale(1, 10000, 10)
+intensity_levels = logscale(2000.0, 75000.0, 1)
+#emissive_levels = logscale(1, 10000, 10)
+cloud_coverage = np.linspace(0, 1, 10)
 # technically, we now need to sample 1000*1000 = 1e6 points
 intensities = []
 emissive_boosts = []
+cloud_coverages = []
 measurements = []
 
 dataconnector.SendJSON({"type":"query"})
@@ -81,6 +84,7 @@ dataconnector.SendJSON({"type":"console", "command":"t.maxFPS 10"})
 dataconnector.SendJSON({"type":"console", "command":"r.PathTracing.ProgressDisplay 0"})
 
 reset_message()
+time.sleep(2.0)
 
 schedule = {"type": "schedule", "command": {
   "type": "query",
@@ -97,16 +101,26 @@ dataconnector.SendJSON(schedule)
 
 current_intensity = 0.0
 current_emissive_boost = 0.0
+current_cc = 0.0
+
+accept_measurements = False
 
 def storage_callback(message) :
   # message like {"type":"query","name":"BPLightMeter_C_1.Intensity","data":{"value":0.000000}}
   global intensities, emissive_boosts, measurements, current_intensity, current_emissive_boost
   #print(message)
+  if not accept_measurements :
+    return
   try :
     message = json.loads(message)
-    measurements.append(float(message["data"]["BPLightMeter_C_1.Intensity"]["data"]["value"]))
+    if message["type"] == "error" :
+      print(message)
+    measurement = float(message["data"]["BPLightMeter_C_1.Intensity"]["data"]["value"])
+    print(measurement)
+    measurements.append(measurement)
     intensities.append(float(message["data"]["SunSky_C_1.DirectionalLight"]["data"]["value"]))
     emissive_boosts.append(current_emissive_boost)
+    cloud_coverages.append(current_cc)
   except Exception as e:
     pass
   #endtry
@@ -115,18 +129,31 @@ def storage_callback(message) :
 dataconnector.SetMessageCallback(storage_callback)
 
 
-for emissive_boost in emissive_levels :
+for cc in cloud_coverage :
   for intensity in intensity_levels :
-    print("Combination of: ", intensity, " Lumen and ", emissive_boost, " Emission")
+    accept_measurements = False
+    print("Combination of: ", intensity, " Lumen and ", cc, " Cloud Coverage")
     current_intensity = intensity
-    current_emissive_boost = emissive_boost
+    current_cc = cc
+    #dataconnector.SendJSON({
+    #  "type":"material",
+    #  "slot":"CallibrationMaterial",
+    #  "object":"StaticMeshActor",
+    #  "dtype":"scalar",
+    #  "parameter":"Emissive",
+    #  "value": float(emissive_boost)
+    #})
     dataconnector.SendJSON({
-      "type":"material",
-      "slot":"CallibrationMaterial",
-      "object":"StaticMeshActor",
-      "dtype":"scalar",
-      "parameter":"Emissive",
-      "value": float(emissive_boost)
+      "type": "parameter",
+      "object": "SunSky",
+      "property": "Coverage",
+      "value": float(cc)
+    })
+    dataconnector.SendJSON({
+      "type": "parameter",
+      "object": "SunSky",
+      "property": "CloudDensity",
+      "value": float(cc)
     })
     dataconnector.SendJSON({
       "type": "parameter",
@@ -134,12 +161,22 @@ for emissive_boost in emissive_levels :
       "property": "Intensity",
       "value": float(intensity)
     })
-    time.sleep(0.5)
+    dataconnector.SendJSON({
+      "type": "call",
+      "object": "SunSky",
+      "method": "ApplySettings",
+    })
+    time.sleep(2.0)
+    accept_measurements = True
+    time.sleep(2.0)
   #endfor
 #endfor
 
 # export to csv
 import pandas as pd
-df = pd.DataFrame({"intensity": intensities, "emissive_boost": emissive_boosts, "measurement": measurements})
+df = pd.DataFrame({"intensity": intensities, "emissive_boost": emissive_boosts, "measurement": measurements, "cloud_coverage": cloud_coverages})
 
 df.to_csv("measurements" + str(time.time()) + ".csv")
+
+dataconnector.SendJSON({"type":"quit"})
+
