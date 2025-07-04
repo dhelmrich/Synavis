20d0c9f854843cee0a9347f8f1e1121539d5fede
Dirk Baker <dnh2@hi.is>
2025-07-04 07:35:49 +0200

    Add Windows build script and enhance Unix build script
    Introduces cmake_install.bat for Windows with argument parsing and build options. Improves cmake_install.sh with more flexible argument handling, optional automatic CPlantBox cloning, and support for selecting the compiler with --clang.

---
+++ cmake_install.bat
@@ -0,0 +1,149 @@
+@echo off
+REM filepath: f:\Work\Synavis\cmake_install.bat
+
+REM Default values
+set "BUILDDIR=build"
+set "BUILDTYPE=Release"
+set "DELBUILD=0"
+set "ACTIVATE_DECODING=0"
+set "VERBOSITY=0"
+set "CPLANTBOX_DIR="
+set "BASEDIR="
+set "NPROC="
+setlocal enabledelayedexpansion
+
+REM Parse arguments
+:parse_args
+if "%~1"=="" goto after_args
+if "%~1"=="-d" (
+    set "BUILDDIR=%~2"
+    shift
+    shift
+    goto parse_args
+) else if "%~1"=="-t" (
+    set "BUILDTYPE=%~2"
+    shift
+    shift
+    goto parse_args
+) else if "%~1"=="-e" (
+    set "DELBUILD=1"
+    shift
+    goto parse_args
+) else if "%~1"=="-j" (
+    set "NPROC=%~2"
+    if not defined NPROC (
+        set "NPROC=1"
+    )
+    shift
+    shift
+    goto parse_args
+) else if "%~1"=="-c" (
+    set "ACTIVATE_DECODING=1"
+    shift
+    goto parse_args
+) else if "%~1"=="-B" (
+    set "BASEDIR=%~2"
+    shift
+    shift
+    goto parse_args
+) else if "%~1"=="-v" (
+    set "VERBOSITY=1"
+    shift
+    goto parse_args
+) else if "%~1"=="-p" (
+    set "CPLANTBOX_DIR=%~2"
+    shift
+    shift
+    goto parse_args
+) else (
+    echo Usage: %0 [-d builddir] [-t buildtype] [-e deletebuild] [-j nproc] [-c activate_decoding] [-B basedir] [-v verbosity] [-p cplantbox_location]
+    echo.
+    echo    -d builddir           Specify the build directory name (default: build)
+    echo    -t buildtype          Specify the build type (default: Release)
+    echo    -e deletebuild        Delete the build directory after building (default: false)
+    echo    -j nproc              Specify the number of processes to use for building (default: all cores minus one)
+    echo    -c activate_decoding  Activate decoding (default: false)
+    echo    -B basedir            Specify the base directory (default: current directory)
+    echo    -v verbosity          Enable verbose logging (default: false)
+    echo    -p cplantbox_location Specify the location of cplantbox (default: not set)
+    exit /b 1
+)
+:after_args
+
+REM Set DIR to script location or BASEDIR
+set "DIR=%~dp0"
+if not "%BASEDIR%"=="" set "DIR=%BASEDIR%"
+
+REM Convert backslashes to forward slashes for CMake
+set "DIR_SLASHED=%DIR:\=/%"
+set "BUILDDIR_SLASHED=%BUILDDIR:\=/%"
+
+REM Remove trailing slash from DIR_SLASHED if present
+if "%DIR_SLASHED:~-1%"=="/" set "DIR_SLASHED=%DIR_SLASHED:~0,-1%"
+
+REM Delete build directory if requested
+if "%DELBUILD%"=="1" (
+    if exist "%DIR%\%BUILDDIR%" (
+        echo Deleting build directory
+        rmdir /s /q "%DIR%\%BUILDDIR%"
+    )
+)
+
+REM Create build directory
+if not exist "%DIR%\%BUILDDIR%" (
+    mkdir "%DIR%\%BUILDDIR%"
+)
+
+REM On Windows, prefer Visual Studio, else fallback to NMake Makefiles if nmake is available
+set "GENERATOR=Visual Studio 17 2022"
+where nmake >nul 2>nul
+if %ERRORLEVEL%==0 (
+  REM Optionally, you could allow override here if needed
+  REM set "GENERATOR=NMake Makefiles"
+)
+
+set "LIBDATACHANNEL_BUILD_TESTS=-DLIBDATACHANNEL_BUILD_TESTS=Off"
+set "LIBDATACHANNEL_BUILD_EXAMPLES=-DLIBDATACHANNEL_BUILD_EXAMPLES=Off"
+set "LIBDATACHANNEL_SETTINGS=-DENABLE_DEBUG_LOGGING=On -DENABLE_LOCALHOST_ADDRESS=On -DENABLE_LOCAL_ADDRESS_TRANSLATION=On"
+set "SYNAVIS_APPBUILD=-DBUILD_WITH_APPS=Off"
+
+REM Get Python include dir and library dir
+for /f "delims=" %%i in ('python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())"') do set "PYTHON_INCLUDE_DIRS=%%i"
+for /f "delims=" %%i in ('python -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))"') do set "PYTHON_LIBRARY=%%i"
+if "%PYTHON_LIBRARY%"=="" (
+    echo Error: Could not determine Python LIBDIR. Please ensure Python is installed with a shared library or set PYTHON_LIBRARY manually.
+    exit /b 1
+)
+echo Python include dir: %PYTHON_INCLUDE_DIRS%
+
+REM Decoding option
+set "DECODING="
+if "%ACTIVATE_DECODING%"=="1" (
+    echo Activating decoding
+    set "DECODING=-DBUILD_WITH_DECODING=On"
+)
+
+REM Verbosity
+set "CMAKE_VERBOSE_LOGGING="
+if "%VERBOSITY%"=="1" (
+    echo Enabling verbose logging
+    set "CMAKE_VERBOSE_LOGGING=-DCMAKE_VERBOSE_MAKEFILE=On"
+)
+
+REM CPlantBox
+set "CPLANTBOX_DIR_OPTION="
+if not "%CPLANTBOX_DIR%"=="" (
+    echo Using cplantbox location: %CPLANTBOX_DIR%
+    set "CPLANTBOX_DIR_OPTION=-DCPlantBox_DIR=%CPLANTBOX_DIR%"
+)
+
+REM Configure
+set "CMAKE_CONFIGURE_CMD=cmake -S %DIR_SLASHED% -B %DIR_SLASHED%/%BUILDDIR_SLASHED% -DCMAKE_BUILD_TYPE=%BUILDTYPE% -G "%GENERATOR%" %LIBDATACHANNEL_BUILD_TESTS% %LIBDATACHANNEL_BUILD_EXAMPLES% %LIBDATACHANNEL_SETTINGS% %DECODING% -DPYTHON_INCLUDE_DIR=%PYTHON_INCLUDE_DIRS% -DPYTHON_LIBRARY=%PYTHON_LIBRARY% %SYNAVIS_APPBUILD% %CMAKE_VERBOSE_LOGGING% %CPLANTBOX_DIR_OPTION%"
+echo Running: %CMAKE_CONFIGURE_CMD%
+%CMAKE_CONFIGURE_CMD%
+
+REM Build
+set "CMAKE_BUILD_CMD=cmake --build %DIR_SLASHED%/%BUILDDIR_SLASHED% --config %BUILDTYPE% -j %NPROC%"
+echo Running: %CMAKE_BUILD_CMD%
+%CMAKE_BUILD_CMD%
+
--- cmake_install.sh
+++ cmake_install.sh
@@ -1,38 +1,72 @@
 # build directory name can be a parameter -d
 # build type can be a parameter -t
+# Default values
 BUILDDIR="build"
 BUILDTYPE="Release"
 DELBUILD=false
-# check number of cores
+ACTIVATE_DECODING=true
 nproc=$(nproc)
 # subtract one
 nproc=$((nproc-1))
 
-while getopts d:t:e:j:c:B:v:p: option
-do
-case "${option}"
-in
-d) BUILDDIR=${OPTARG};;
-t) BUILDTYPE=${OPTARG};;
-e) DELBUILD=true;;
-j) nproc=${OPTARG};;
-c) ACTIVATE_DECODING=false;;
-B) BASEDIR=${OPTARG};;
-v) VERBOSITY=${OPTARG};;
-p) CPLANTBOX_DIR=${OPTARG};;
-*) echo "Usage: $0 [-d builddir] [-t buildtype] [-e deletebuild] [-j nproc] [-c activate_decoding] [-B basedir] [-v verbosity] [-p cplantbox_location]"
-   echo "  -d builddir       Specify the build directory name (default: build)"
-   echo "  -t buildtype     Specify the build type (default: Release)"
-   echo "  -e deletebuild    Delete the build directory after building (default: false)"
-   echo "  -j nproc          Specify the number of processes to use for building (default: nproc - 1)"
-   echo "  -c activate_decoding  Activate decoding (default: false)"
-   echo "  -B basedir      Specify the base directory (default: current directory)"
-   echo "  -v verbosity      Enable verbose logging (default: false)"
-   echo "  -p cplantbox_location  Specify the location of cplantbox (default: not set)"
-   exit 1
-   ;;
-esac
+# Parse arguments manually
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -d) BUILDDIR="$2"; shift 2;;
+    -t) BUILDTYPE="$2"; shift 2;;
+    -e)
+      # Accept -e or -e true/false
+      if [[ "$2" == "true" ]]; then
+        DELBUILD=true; shift 2
+      elif [[ "$2" == "false" ]]; then
+        DELBUILD=false; shift 2
+      else
+        DELBUILD=true; shift
+      fi
+      ;;
+    -j) nproc="$2"; shift 2;;
+    -c)
+      # Accept -c or -c true/false
+      if [[ "$2" == "true" ]]; then
+        ACTIVATE_DECODING=true; shift 2
+      elif [[ "$2" == "false" ]]; then
+        ACTIVATE_DECODING=false; shift 2
+      else
+        ACTIVATE_DECODING=false; shift
+      fi
+      ;;
+    -B) BASEDIR="$2"; shift 2;;
+    -v) VERBOSITY="$2"; shift 2;;
+    -p) CPLANTBOX_DIR="$2"; shift 2;;
+    --clang) USE_CLANG=true; shift;;
+    -h|--help)
+      echo "Usage: $0 [-d builddir] [-t buildtype] [-e deletebuild] [-j nproc] [-c activate_decoding] [-B basedir] [-v verbosity] [-p cplantbox_location] [--clang]"
+      echo "  -d builddir       Specify the build directory name (default: build)"
+      echo "  -t buildtype      Specify the build type (default: Release)"
+      echo "  -e deletebuild    Delete the build directory after building (default: false, accepts optional true/false)"
+      echo "  -j nproc          Specify the number of processes to use for building (default: nproc - 1)"
+      echo "  -c activate_decoding  Activate decoding (default: false, accepts optional true/false)"
+      echo "  -B basedir        Specify the base directory (default: current directory)"
+      echo "  -v verbosity      Enable verbose logging (default: false)"
+      echo "  -p cplantbox_location  Specify the location of cplantbox (default: not set)"
+      echo "  --clang           Use clang as the C/C++ compiler"
+      exit 0
+      ;;
+    *)
+      echo "Unknown option: $1"
+      echo "Use -h or --help for usage."
+      exit 1
+      ;;
+  esac
 done
+
+CMAKEOPT=""
+
+# Set compiler if --clang is used
+if [ "$USE_CLANG" = true ]; then
+  echo "Using clang as compiler"
+  CMAKEOPT="$CMAKEOPT -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
+fi
 
 if [ "$DELBUILD" = true ] ; then
   echo "Deleting build directory"
@@ -47,7 +81,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ -n "$BASEDIR" ]; then
   DIR=$BASEDIR
 fi
-
 
 # create build directory
 mkdir -p $DIR/$BUILDDIR
@@ -88,11 +121,20 @@ if [ -n "$CPLANTBOX_DIR" ]; then
   echo "Using cplantbox location: $CPLANTBOX_DIR"
   CPLANTBOX_DIR_OPTION="-DCPlantBox_DIR=$CPLANTBOX_DIR"
 else
-  echo "No cplantbox location specified, using default."
+  echo "No cplantbox location specified, pulling CPlantBox from git"
+  # Check if CPlantBox is already cloned
+  if [ ! -d "$DIR/CPlantBox" ]; then
+    echo "Cloning CPlantBox repository"
+    git clone https://github.com/Plant-Root-Soil-Interactions-Modelling/CPlantBox.git
+  else
+    echo "Found existing CPlantBox directory, using it"
+  fi
+  # Set the CPlantBox_DIR to the cloned directory
+  CPLANTBOX_DIR_OPTION="-DCPlantBox_DIR=$DIR/CPlantBox/build"
 fi
 
 # configure
-cmake -H$DIR -B$DIR/$BUILDDIR -DCMAKE_BUILD_TYPE=$BUILDTYPE -G "$GENERATOR" $LIBDATACHANNEL_VERBOSELOGGING $LIBDATACHANNEL_BUILD_TESTS $LIBDATACHANNEL_BUILD_EXAMPLES $LIBDATACHANNEL_SETTINGS $DECODING -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIRS -DPYTHON_LIBRARY=$PYTHON_LIBRARY $SYNAVIS_APPBUILD $CMAKE_VERBOSE_LOGGING $CPLANTBOX_DIR_OPTION
+cmake -H$DIR -B$DIR/$BUILDDIR -DCMAKE_BUILD_TYPE=$BUILDTYPE -G "$GENERATOR" $LIBDATACHANNEL_VERBOSELOGGING $LIBDATACHANNEL_BUILD_TESTS $LIBDATACHANNEL_BUILD_EXAMPLES $LIBDATACHANNEL_SETTINGS $DECODING -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIRS -DPYTHON_LIBRARY=$PYTHON_LIBRARY $SYNAVIS_APPBUILD $CMAKE_VERBOSE_LOGGING $CPLANTBOX_DIR_OPTION $CMAKEOPT
 
 # build
 cmake --build $DIR/$BUILDDIR -- -j $nproc
